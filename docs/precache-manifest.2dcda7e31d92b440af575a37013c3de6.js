c62b4f4c34ace1026453247680b456a3615f6fc8
Sebastian Banaszkiewicz <banaszkiewicz.sebastian@gmail.com>
2020-04-20 05:12:22 +0200

    homepage added to package.json

---
+++ docs/precache-manifest.2dcda7e31d92b440af575a37013c3de6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a643be9241fc4832cbe9a5d62fc6155",
+    "url": "/temperature-converter/index.html"
+  },
+  {
+    "revision": "7509643655b74b3fcf4c",
+    "url": "/temperature-converter/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "34fca85953717be0de26",
+    "url": "/temperature-converter/static/js/2.6b8fa38a.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/temperature-converter/static/js/2.6b8fa38a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7509643655b74b3fcf4c",
+    "url": "/temperature-converter/static/js/main.e52142e4.chunk.js"
+  },
+  {
+    "revision": "73e61872369d37a88dda",
+    "url": "/temperature-converter/static/js/runtime-main.92c7d47b.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.373d6bd3fbef63b95d318dd856db4bb5.js"
+  "/temperature-converter/precache-manifest.2dcda7e31d92b440af575a37013c3de6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/temperature-converter/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
